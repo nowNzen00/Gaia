0d077af198130c81ca23ec29e33b583d28b38adf
Jose Argueta <jajose780@gmail.com>
2021-10-10 22:22:49 -0700

    body skeleton complete

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,7 +1,15 @@
 import "./Body.css";
 
 function Body() {
-  return <div className="Body"></div>;
+  return (
+    <div className="Body">
+      <div className="city-name">City Name</div>
+      <div className="date">Day/Date</div>
+      <div className="temperature">temperature</div>
+      <div className="weather">Weather</div>
+      <div className="highest-lowest">ratio</div>
+    </div>
+  );
 }
 
 export default Body;
